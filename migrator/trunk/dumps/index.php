9bea8964b6f5043cce2eb4aab6bc2fac5ab228d7
Sam Moffatt <pasamio@users.noreply.github.com>
2007-05-21 11:34:41 +0000

    Initial Import

---
+++ migrator/trunk/dumps/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//empty file
+//thanks for looking, which actually was a complete waste of time
+//just like keeping on reading this
+//I would stop; nothing beyond this point
+//
+//no, really.... Go Away!!!
+
+?>
