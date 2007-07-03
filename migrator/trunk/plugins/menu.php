10c57c0c2d61d53b82f0c2b2b544fcbbf546d67c
Sam Moffatt <pasamio@users.noreply.github.com>
2007-07-03 06:54:28 +0000

    Menu Migration! Mostly working!

--- migrator/trunk/plugins/menu.php
+++ migrator/trunk/plugins/menu.php
@@ -27,6 +27,41 @@ class Menu_ETL extends ETLPlugin {
 	
 	function mapvalues($key,$value) {
 		switch($key) {
+			case 'params':
+				$value = str_replace('readmore=','show_readmore=',$value);
+				$value = str_replace('pdf=','show_pdf_icon=',$value);
+				$value = str_replace('print=','show_print_icon=',$value);
+				$value = str_replace('email=','show_email_icon=',$value);
+				$value = str_replace('leading=','num_leading_articles=',$value);
+				$value = str_replace('page_title=','show_page_title=',$value);
+				$value = str_replace('header=','page_title=',$value);
+				$value = str_replace('intro=','num_intro_articles=',$value);
+				$value = str_replace('columns=','num_columns=',$value);
+				$value = str_replace('link=','num_links=',$value);
+				$value = str_replace('pagination=','show_pagination=',$value);
+				$value = str_replace('pagination_results=','show_pagination_results=',$value);
+				$value = str_replace('item_title=','show_title=',$value);
+				$value = str_replace('category=','show_category=',$value);
+				$value = str_replace('category_link=','link_category=',$value);
+				$value = str_replace('rating=','show_vote=',$value);
+				$value = str_replace('createdate=','show_create_date=',$value);
+				$value = str_replace('modifydate=','show_modify_date=',$value);
+				$value = str_replace('description=','show_description=',$value);
+				$value = str_replace('description_image=','show_description_image=',$value);
+				$value = str_replace('introtext=','show_intro=',$value);
+				$value = str_replace('section=','show_section=',$value);
+				$value = str_replace('section_link=','link_section=',$value);
+				$value = str_replace('description_cat=','show_category_description=',$value);
+				$value = str_replace('date=','show_date=',$value);
+				$value = str_replace('hits=','show_hits=',$value);
+				$value = str_replace('headings=','show_headings=',$value);
+				$value = str_replace('empty_cat=','show_empty_categories=',$value);
+				$value = str_replace('other_cat=','show_categories=',$value);
+				$value = str_replace('cat_items=','show_cat_num_articles=',$value);
+				$value = str_replace('display=','show_pagination_limit=',$value);
+				$value = str_replace('navigation=','show_item_navigation=',$value);
+				return $value;
+				break;
 			default:
 				return $value;
 				break;
