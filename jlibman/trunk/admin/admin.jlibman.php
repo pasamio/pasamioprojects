96bc9240cd919fa1d676afb02a09ad004477680e
Sam Moffatt <pasamio@users.noreply.github.com>
2007-09-21 06:15:44 +0000

    Initial import.

---
+++ jlibman/trunk/admin/admin.jlibman.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Joomla! Library Manager
+ * 
+ * The Joomla! Library Manager installs libraries 
+ * 
+ * PHP4/5
+ *  
+ * Created on Sep 21, 2007
+ * 
+ * @package jlibman
+ * @author Sam Moffatt <S.Moffatt@toowoomba.qld.gov.au>
+ * @author Toowoomba City Council Information Management Branch
+ * @license GNU/GPL http://www.gnu.org/licenses/gpl.html
+ * @copyright 2007 Toowoomba City Council/Developer Name 
+ * @version SVN: $Id:$
+ * @see JoomlaCode Project: http://joomlacode.org/gf/project/tccprojects
+ */
+
+// no direct access
+defined(_JEXEC) or die('No direct access allowed ;)');
+
+?>
