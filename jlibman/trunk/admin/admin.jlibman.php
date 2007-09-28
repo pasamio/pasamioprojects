824ebf466bc83d995b3bec01630a5a6740b287fa
Sam Moffatt <pasamio@users.noreply.github.com>
2007-09-28 04:20:33 +0000

    Filling out the component

--- jlibman/trunk/admin/admin.jlibman.php
+++ jlibman/trunk/admin/admin.jlibman.php
@@ -18,6 +18,28 @@
  */
 
 // no direct access
-defined(_JEXEC) or die('No direct access allowed ;)');
+defined('_JEXEC') or die('No direct access allowed ;)');
 
+// Require the base controller
+require_once( JPATH_COMPONENT.DS.'controller.php' );
+
+// Require specific controller if requested
+if($controller = JRequest::getWord('controller')) {
+    $path = JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php';
+    if (file_exists($path)) {
+        require_once $path;
+    } else {
+        $controller = '';
+    }
+}
+
+// Create the controller
+$classname    = 'JLibManController'.$controller;
+$controller   = new $classname( );
+
+// Perform the Request task
+$controller->execute( JRequest::getVar( 'task' ) );
+
+// Redirect if set by the controller
+$controller->redirect();
 ?>
---
+++ jlibman/trunk/admin/models/jlibman.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Document Description
+ * 
+ * Document Long Description 
+ * 
+ * PHP4/5
+ *  
+ * Created on Sep 28, 2007
+ * 
+ * @package package_name
+ * @author Your Name <author@toowoomba.qld.gov.au>
+ * @author Toowoomba City Council Information Management Branch
+ * @license GNU/GPL http://www.gnu.org/licenses/gpl.html
+ * @copyright 2007 Toowoomba City Council/Developer Name 
+ * @version SVN: $Id:$
+ * @see Project Documentation DM Number: #???????
+ * @see Gaza Documentation: http://gaza.toowoomba.qld.gov.au
+ * @see JoomlaCode Project: http://joomlacode.org/gf/project/
+ */
+ 
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+jimport( 'joomla.application.component.model' );
+
+/**
+ * Hello Model
+ *
+ * @package    Joomla.Tutorials
+ * @subpackage Components
+ */
+class JLibManModelJLibMan extends JModel
+{
+    /**
+    * Gets the greeting
+    * @return string The greeting to be displayed to the user
+    */
+    function getGreeting()
+    {
+           $db =& JFactory::getDBO();
+
+		   $query = 'SELECT title FROM #__content';
+		   $db->setQuery( $query );
+		   $greeting = $db->loadResult();
+		
+		   return $greeting;
+    }
+}
+?>
