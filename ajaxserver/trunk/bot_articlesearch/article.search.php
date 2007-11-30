98e01a6ef08dab51d7a0675a57c439363540dd33
Sam Moffatt <pasamio@users.noreply.github.com>
2007-11-30 05:24:13 +0000

    Fix for IE issues with search plugin

--- ajaxserver/trunk/bot_articlesearch/article.search.php
+++ ajaxserver/trunk/bot_articlesearch/article.search.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* AJAX Article Search
-* @author Samuel Moffatt <pasamio@pasamio.id.au>
-* @copyright Copyright (C) 2006 Samuel Moffatt. All rights reserved
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
-*/
+ * AJAX Article Search
+ * @author Samuel Moffatt <pasamio@pasamio.id.au>
+ * @copyright Copyright (C) 2006 Samuel Moffatt. All rights reserved
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ */
 
 /** ensure this file is being included by a parent file */
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
@@ -12,13 +12,13 @@ defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.'
 $_MAMBOTS->registerFunction( 'onGetAJAXServices', 'ajaxGetArticleSearch' );
 
 function ajaxGetArticleSearch() {
-        return array(
-                array(
-                        'name' => 'search.articles',
-                        'method' => 'ajaxDoArticleSearch',
-                        'help' => 'Searches articles on the site',
-                ),
-        );
+	return array(
+	array(
+	'name' => 'search.articles',
+	'method' => 'ajaxDoArticleSearch',
+	'help' => 'Searches articles on the site',
+	),
+	);
 }
 
 
@@ -34,35 +34,36 @@ function ajaxDoArticleSearch($query,$field='contentid') {
 	$now = date( 'Y-m-d H:i:s', time()+$mosConfig_offset*60*60 );
 	$noauth = !$mainframe->getCfg( 'shownoauth' );
 	$nullDate = $database->getNullDate();
-	
+
 	$query = "SELECT id, title, introtext"
 		."\n FROM #__content"
 		."\n WHERE state != 0"
 		."\n AND title LIKE '%".strtolower($search)."%'"
 		."\n AND (publish_up = '$nullDate' OR publish_up <= '$now' ) "
-		."\n AND (publish_down = '$nullDate' OR publish_down >= '$now' )"
-		."\n ORDER BY title ASC";
-	$database->setQuery($query);		
+	."\n AND (publish_down = '$nullDate' OR publish_down >= '$now' )"
+	."\n ORDER BY title ASC";
+	$database->setQuery($query);
 	$results = $database->loadAssocList();
 	if(count($results)) {
 		//Bust the returned rows into an array for easy usage
-		$retval .= '<select name="created_by" class="inputbox">';
-	        foreach($results as $result) {
+		$retval .= '<select name="created_by" class="inputbox" onClick="if(this.options.length < 2) { eval(this.options[this.selectedIndex].getAttribute(\'action\')); }" onChange="eval(this.options[this.selectedIndex].getAttribute(\'action\'));">';
+		foreach($results as $result) {
 			if(trim($result['title']) != '' && in_array($result['title'],$result)) {
 				$uid = $result['id'];
-				$retval .= "<option onClick=\"document.getElementById('preview').innerHTML='".
-				str_replace("\r\n",'',str_replace('"','',str_replace("'",'',$result['introtext'])))
-				."'; document.getElementById('".$field."').value='".$uid."'; lastsearchstring='".$result['title']."'\" value=\"".$uid."\">".$result['title']."</option>";
-			}			
+				$action = "document.getElementById('preview').innerHTML='".
+					str_replace("\r\n",'',str_replace('"','',str_replace("'",'',$result['introtext'])))
+					."'; document.getElementById('".$field."').value='".$uid."'; lastsearchstring='".$result['title']."'";
+				$retval .= "<option action=\"". $action ."\" value=\"".$uid."\">".$result['title']."</option>";
+			}
 		}
 		$retval .= '</select>';
 	} else {
-	        $retval .= 'Not Found: '. $search;
-    	}
-        // Output data to client
+		$retval .= 'Not Found: '. $search;
+	}
+	// Output data to client
 	$objResponse = new xajaxResponse();
-        $objResponse->addAssign("results","innerHTML", $retval);
-                                                                                                    
-        return $objResponse->getXML();
+	$objResponse->addAssign("results","innerHTML", $retval);
+
+	return $objResponse->getXML();
 }
 
