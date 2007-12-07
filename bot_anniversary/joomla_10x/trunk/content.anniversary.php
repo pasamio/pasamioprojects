194809f637030d697a9b8090e958ffa74a455aa4
Sam Moffatt <pasamio@users.noreply.github.com>
2007-12-07 03:23:15 +0000

    Initial import.

---
+++ bot_anniversary/joomla_10x/trunk/content.anniversary.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Document Description
+ * 
+ * Document Long Description 
+ * 
+ * PHP4/5
+ *  
+ * Created on Dec 7, 2007
+ * 
+ * @package package_name
+ * @author Your Name <author@toowoomba.qld.gov.au>
+ * @author Toowoomba City Council Information Management Branch
+ * @license GNU/GPL http://www.gnu.org/licenses/gpl.html
+ * @copyright 2007 Toowoomba City Council/Developer Name 
+ * @version SVN: $Id:$
+ * @see Project Documentation DM Number: #???????
+ * @see Gaza Documentation: http://gaza.toowoomba.qld.gov.au
+ * @see JoomlaCode Project: http://joomlacode.org/gf/project/
+ */
+ 
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+
+$_MAMBOTS->registerFunction( 'onPrepareContent', 'botAnniversaryReplacer' );
+
+/**
+*/
+function botAnniversaryReplacer( $published, &$row, &$params, $page=0 ) {
+	global $database, $_MAMBOTS;
+	
+	// simple performance check to determine whether bot should process further
+	if ( strpos( $row->text, 'anniversary' ) === false ) {
+		return true;
+	}
+	
+ 	// expression to search for
+	$regex = '/{anniversary\s*.*?}/i';	
+	$row->text = preg_replace_callback( $regex, 'botanniversaryReplacerCalculator', $row->text );
+}
+
+function botAnniversaryReplacerCalculator(&$matches) {
+	$d1 = str_replace('}','',str_replace('{anniversary ','',$matches[0]));
+	$parts = explode('-',$d1);
+	$yx = date('Y',time());
+	$mx = date('m',time());
+	$dx = date('d',time());
+	$yeardiff = $yx - $parts[0];
+	if($mx < $parts[1]) $yeardiff--;
+	else if($dx < $parts[2]) $yeardiff--;
+	return $yeardiff;
+}
+
+
