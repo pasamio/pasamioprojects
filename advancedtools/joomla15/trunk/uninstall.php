c583a0c2e4f28c5f3cd6059ab230c040701010c8
Sam Moffatt <pasamio@users.noreply.github.com>
2007-12-14 06:31:32 +0000

    Updated uninstall code

--- advancedtools/joomla15/trunk/uninstall.php
+++ advancedtools/joomla15/trunk/uninstall.php
@@ -2,7 +2,7 @@
 /**
  * Advanced Tools Uninstaller
  * 
- * Does nothing 
+ * Removes module as well
  * 
  * PHP4/5
  *  
@@ -55,5 +55,5 @@ function _getExtensionID($type, $id, $client, $group) {
 // However we do uninstall the module manager
 $tmpinstaller = new JInstaller();
 $result = $tmpinstaller->uninstall('module', _getExtensionID('module','mod_advmenu', 'administrator','menu'), 1 );
-echo "Please note that this component has uninstalled the Advanced Menu module as well.";
-if(!$result) echo "Automated uninstallation of the module failed."; 
+echo JText::_("Please note that this component has uninstalled the Advanced Menu module as well.");
+if(!$result) echo JText::_("Automated uninstallation of the module failed."); 
