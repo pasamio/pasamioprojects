b6ba20c9aea57f59aefa0aa7f52c8f61ce72bf0c
Sam Moffatt <pasamio@users.noreply.github.com>
2008-03-28 05:57:58 +0000

    User source checker progressing

--- jdiagnostic/joomla15/trunk/administrator/diagnostics/usersourcechecker/usersourcechecker.php
+++ jdiagnostic/joomla15/trunk/administrator/diagnostics/usersourcechecker/usersourcechecker.php
@@ -18,13 +18,42 @@
  * @see Gaza Documentation: http://gaza.toowoomba.qld.gov.au
  * @see JoomlaCode Project: http://joomlacode.org/gf/project/
  */
- 
+
+class JAuthUserSource extends JObservable {
+
+	/** @var options array options */
+	var $_options;
+
+	/**
+	 * Constructor
+	 *
+	 * @access protected
+	 */
+	function __construct($options=Array()) {
+		// Import User Source Library Files
+		$isLoaded = JPluginHelper :: importPlugin('usersource');
+		if (!$isLoaded) {
+			JError :: raiseWarning('SOME_ERROR_CODE', 'JAuthUserSource::__construct: Could not load User Source plugins.');
+		}
+		$this->_options = $options;
+	}
+}
+
 $username = JRequest::getVar('username','');
 if(strlen($username)) {
-
+	
 }
 ?>
+<h1>User Source Checker</h1>
 <p>This tool takes a user name and attempts to determine if SSO and User autocreation would work for that user.</p>
 <p>Enter the username you wish to examine</p>
-<p>Username: <input type="text" name="username" value="" /></p>
-<p>Attempt Autocreation: <input type="checkbox" name="autocreation" /></p>
+<form method="post" action="index.php">
+<input type="hidden" name="option" value="com_jdiagnostic" />
+<input type="hidden" name="mode" value="diag" />
+<input type="hidden" name="diag" value="usersourcechecker" />
+<table>
+<tr><td>Username</td><td><input type="text" name="username" value="" /></td><td>(This is the name that SSO should have detected)</tr>
+<tr><td>Attempt Autocreation:</td><td><input type="checkbox" name="autocreation" /></td><td>(This will automatically create the user in the system if they don't exist)</td></tr>
+<tr><td colspan="2"><input type="submit" value="Check User" /></td></tr>
+</table>
+</form>
