f01cd15a36c382686f4481d0aed3933addd49274
Sam Moffatt <pasamio@users.noreply.github.com>
2009-01-14 04:53:01 +0000

    Added basic gforge app and connector

---
+++ gforge-apps/jupgrader-xml-builder/builder.php
@@ -0,0 +1,70 @@
+<?php
+include(dirname(__FILE__).'/../common/user_config.php');
+include(dirname(__FILE__).'/../common/gforgeconnector.php');
+$config = new Config();
+$client = new GForgeConnector($config->site);
+$client->login($config->username, $config->password);
+//$user = $client->getUser();
+$project = $client->getProject('joomla');
+$packages = $client->getFrsPackages($project->project_id);
+// Mock this below, creation once is enough
+//$frs_package_id = $client->addFrsPackage($project->project_id, 'Test SOAP Package', 1, 1, 1);
+//$frs_package_id = 4059; // TestSOAPPackage (write testing)
+//$frs_package_id = 2588; // Migrator (read testing)
+
+//$frs_release_id = $client->addFrsRelease($frs_package_id, 'Test', 'This is a test release', 'Testing SOAP');
+//$frs_release_id = 8707; // Test release for TestSOAPPackage
+//$filename = 'Meat_pie-small.png';
+//var_dump($client->addFilesystem('frsrelease', $frs_release_id, $filename, '', base64_encode(file_get_contents($filename))));
+//$filesystem_id = 32092;
+//var_dump($client->getFilesystem($filesystem_id));
+$last = null;
+
+foreach($packages as $package) {
+	if($last == null) {
+		$last = $package;
+	} else {
+		if($package->is_public && $package->status_id && $package->frs_package_id > $last->frs_package_id && strpos($package->package_name, '1.5')) {
+			$last = $package;
+		}
+	}	
+}
+
+if($last) {
+	$package = $last;
+	header('Content-type: text/xml');
+	$joomla_version = str_replace('Joomla', '', $package->package_name);
+	echo '<?xml version="1.0" ?>'."\n";
+	echo '<jupgrader release="'. $joomla_version .'">'."\n";
+	$releases = $client->getFrsReleases($package->frs_package_id);
+	
+	foreach($releases as $release) {
+		$update = 0;
+		if(strpos($release->release_name, 'updates')) {
+			$update = 1;
+			echo '<patches>'."\n";
+		}
+		$files = $client->getFilesystems('frsrelease', $release->frs_release_id);
+		foreach($files as $file) {
+			if(substr($file->file_name, -6) == 'tar.gz') {
+				if($update) {
+					$version_parts = explode('_', $file->file_name);
+					echo "\t".'<patchpackage version="'. $version_parts[1] .'" url="http://joomlacode.org'. $file->download_url.'" filename="'. $file->file_name_safe .'" filesize="'. $file->file_size .'" />'."\n";	
+				} else {
+					echo '<fullpackage url="http://joomlacode.org'. $file->download_url .'" filename="'. $file->file_name_safe.'" filesize="'. $file->file_size .'" />'."\n";
+				}
+			}
+		}
+		if($update) {
+			echo  '</patches>'."\n";
+		}
+	}
+	echo '</jupgrader>'."\n";
+} else {
+	echo 'Failed to find a valid package';
+}
+
+$client->logout();
+
+
+//*/
---
+++ gforge-apps/uploader/uploader.php
@@ -0,0 +1,32 @@
+#!/usr/bin/env php
+<?php
+
+if($argc < 3) {
+	die("Usage: php uploader.php section identifier filenames\n");
+}
+
+$valid_sections = Array('frsrelease');
+$args = $argv; // make a copy of this
+array_shift($args); // remove the file name
+$section = array_shift($args);
+$frs_release_id = intval(array_shift($args));
+
+include(dirname(__FILE__).'/../common/user_config.php');
+include(dirname(__FILE__).'/../common/gforgeconnector.php');
+$config = new Config();
+$client = new GForgeConnector($config->site);
+$client->login($config->username, $config->password);
+
+
+foreach($args as $filename) {
+		$result = $client->addFilesystem('frsrelease', $frs_release_id, basename($filename), '', base64_encode(file_get_contents($filename)));
+		if($result) {
+			echo "Added new filesystem object $result\n";	
+		} else {
+			echo "\n";
+		}
+}
+
+
+$client->logout();
+
