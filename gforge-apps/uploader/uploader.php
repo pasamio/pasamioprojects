07887b52f454cbd0aeb23e1bcfd35be491821df6
Sam Moffatt <pasamio@users.noreply.github.com>
2009-01-19 07:06:07 +0000

    Fixed up error reporting code

--- gforge-apps/uploader/uploader.php
+++ gforge-apps/uploader/uploader.php
@@ -15,15 +15,15 @@ include(dirname(__FILE__).'/../common/user_config.php');
 include(dirname(__FILE__).'/../common/gforgeconnector.php');
 $config = new Config();
 $client = new GForgeConnector($config->site);
-$client->login($config->username, $config->password);
+$client->login($config->username, $config->password) or die('Error logging in: '. $client->getError() ."\n");
 
 
 foreach($args as $filename) {
 		$result = $client->addFilesystem('frsrelease', $frs_release_id, basename($filename), '', base64_encode(file_get_contents($filename)));
 		if($result) {
 			echo "Added new filesystem object $result\n";	
-		} else {
-			echo "\n";
+		} else {		
+			echo 'Error:'. $client->getError() ."\n";
 		}
 }
 
